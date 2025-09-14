286761eacda8093cb41e4645b07725510f141b44
Jay <joeljoshy224@gmail.com>
2025-09-14 16:31:18 +0000

    okey the document says paste this on the Step 1 Add this code to your w

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,15 +23,20 @@ export default function RootLayout({
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap"
           rel="stylesheet"
         />
-        <Script
-          async
-          id="klaviyo-js"
-          src="https://static.klaviyo.com/analytics/js/klaviyo.js?company_id=VMR4v3"
-        ></Script>
       </head>
       <body className={cn('font-body antialiased')}>
         {children}
         <Toaster />
+        <Script
+          async
+          id="klaviyo-onsite-js"
+          src="https://static.klaviyo.com/onsite/js/VMR4v3/klaviyo.js?company_id=VMR4v3"
+        ></Script>
+        <Script id="klaviyo-init-js">
+          {`
+            !function(){if(!window.klaviyo){window._klOnsite=window._klOnsite||[];try{window.klaviyo=new Proxy({},{get:function(n,i){return"push"===i?function(){var n;(n=window._klOnsite).push.apply(n,arguments)}:function(){for(var n=arguments.length,o=new Array(n),w=0;w<n;w++)o[w]=arguments[w];var t="function"==typeof o[o.length-1]?o.pop():void 0,e=new Promise((function(n){window._klOnsite.push([i].concat(o,[function(i){t&&t(i),n(i)}]))}));return e}}})}catch(n){window.klaviyo=window.klaviyo||[],window.klaviyo.push=function(){var n;(n=window._klOnsite).push.apply(n,arguments)}}}}();
+          `}
+        </Script>
       </body>
     </html>
   );
